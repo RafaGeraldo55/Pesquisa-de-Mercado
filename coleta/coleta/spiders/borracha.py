77150d3285f8c13403e6da92c0c76d1ef72f1d32
Rafa_Geraldo <rafael_geraldo@outlook.com>
2025-09-04 18:37:13 -0300

    Atualizacao para correr todas as paginas

--- coleta/coleta/spiders/borracha.py
+++ coleta/coleta/spiders/borracha.py
@@ -5,6 +5,9 @@ class BorrachaSpider(scrapy.Spider):
     name = "borracha"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/borracha-porta-geladeira?psafe_param=1"]
+    page_count = 1
+    max_page = 15
+
 
     def parse(self, response):
 
@@ -13,8 +16,15 @@ class BorrachaSpider(scrapy.Spider):
         for product in products:
             yield {
                 'description': product.css('a.poly-component__title::text').get(),
-                'currency': product.css('span.andes-money-amount__currency-symbol').get(),
-                'price': product.css('span.andes-money-amount__fraction').get(),
-                'cents': product.css('span.andes-money-amount__cents andes-money-amount__cents--superscript-24').get()
+                'currency': product.css('span.andes-money-amount__currency-symbol::text').get(),
+                'price': product.css('span.andes-money-amount__fraction::text').get(),
+                'cents': product.css('span.andes-money-amount__cents.andes-money-amount__cents--superscript-24::text').get()
             }
+
+        if self.page_count < self.max_page:
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next.a::attr(href)').get()
+            if next_page:
+                self.page_count =+ 1
+                yield scrapy.Request(url=next_page, callback=self.parse)
         pass
+ 
