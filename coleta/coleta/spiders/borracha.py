8c5f2e3759c3ddafd3ff13e059b1ff8a70258ed4
Rafa_Geraldo <rafael_geraldo@outlook.com>
2025-09-04 22:58:17 -0300

    Atualizacao para solucao de problemas

--- coleta/coleta/spiders/borracha.py
+++ coleta/coleta/spiders/borracha.py
@@ -8,24 +8,23 @@ class BorrachaSpider(scrapy.Spider):
     page_count = 1
     max_page = 10
 
-
     def parse(self, response):
 
         products = response.css('div.ui-search-result__wrapper')
 
         for product in products:
 
-            prices = response.css()     
+            prices = product.css('span.andes-money-amount__fraction::text').getall()
 
             yield {
-                'description': product.css('a.poly-component__title::text').get(),
+                'name': product.css('a.poly-component__title::text').get(),
                 'currency': product.css('span.andes-money-amount__currency-symbol::text').get(),
                 'price': product.css('span.andes-money-amount__fraction::text').get(),
-                'cents': product.css('span.andes-money-amount__cents.andes-money-amount__cents--superscript-24::text').get()
+                'cents': product.css('span.andes-money-amount__cents andes-money-amount__cents--superscript-24::text').get()
             }
 
         if self.page_count < self.max_page:
-            next_page = response.css('li.andes-pagination__button andes-pagination__button--next.a::attr(href)').get()
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
             if next_page:
                 self.page_count += 1
                 yield scrapy.Request(url=next_page, callback=self.parse)
